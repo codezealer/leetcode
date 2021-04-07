959fab6be75f24ec1676a901aee96095ac7fcd8c
codezealer <codezealer@gmail.com>
2021-04-07 10:24:45 +0800

    init commit

---
+++ src/test/java/me/codezealer/leetcode/LeetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.codezealer.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
