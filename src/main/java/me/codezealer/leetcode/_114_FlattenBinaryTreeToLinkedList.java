1137f411906ed17262d9125cbc19f71098028eda
codezealer <codezealer@gmail.com>
2021-04-07 14:10:55 +0800

    二叉树转链表

--- src/main/java/me/codezealer/leetcode/_114_FlattenBinaryTreeToLinkedList.java
+++ src/main/java/me/codezealer/leetcode/_114_FlattenBinaryTreeToLinkedList.java
@@ -11,6 +11,22 @@ package me.codezealer.leetcode;
 public class _114_FlattenBinaryTreeToLinkedList {
 
     public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+        root.right = left;
+
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
 
     }
 }
