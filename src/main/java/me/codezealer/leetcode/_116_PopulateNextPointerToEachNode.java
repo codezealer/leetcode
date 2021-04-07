db1a8fee5f6c992d86db85e57eba91bb4714182f
codezealer <codezealer@gmail.com>
2021-04-07 14:21:51 +0800

    '连接各个节点'

---
+++ src/main/java/me/codezealer/leetcode/_116_PopulateNextPointerToEachNode.java
@@ -0,0 +1,58 @@
+package me.codezealer.leetcode;
+
+/**
+ * You are given a perfect binary tree where all leaves are on the same level,
+ * and every parent has two children. The binary tree has the following definition:
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * Populate each next pointer to point to its next right node.
+ * If there is no next right node, the next pointer should be set to NULL.
+ * Initially, all next pointers are set to NULL.
+
+ */
+
+public class _116_PopulateNextPointerToEachNode {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    public void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        node1.next = node2;
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+        connectTwoNode(node1.right, node2.left);
+    }
+
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
