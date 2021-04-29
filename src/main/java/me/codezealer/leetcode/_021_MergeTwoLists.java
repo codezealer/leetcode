6fc45c687e0a0da62e99c7ae2f3269115c672923
codezealer <codezealer@gmail.com>
2021-04-29 13:48:14 +0800

    合并两个有序链表

---
+++ src/main/java/me/codezealer/leetcode/_021_MergeTwoLists.java
@@ -0,0 +1,40 @@
+package me.codezealer.leetcode;
+
+/**
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ * 提示：
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ */
+
+public class _021_MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode lastNode = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+        lastNode.next = l1 != null ? l1 : l2;
+        return res.next;
+    }
+
+}
